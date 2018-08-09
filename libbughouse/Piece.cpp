68e3e0a75cf56cdef85c975f2c6cb9ec6de28eef
Simon Andrews <simon.andrews212@gmail.com>
2018-08-09 12:23:57 -0400

    Add piece class

---
+++ libbughouse/Piece.cpp
@@ -0,0 +1,11 @@
+#include "Piece.h"
+
+Team Piece::getTeam()
+{
+  return team;
+}
+
+PieceType Piece::getType()
+{
+  return type;
+}
---
+++ libbughouse/include/Piece.h
@@ -0,0 +1,31 @@
+#ifndef BUGHOUSE_PIECE_H
+#define BUGHOUSE_PIECE_H
+
+typedef enum
+{
+  Black,
+  White,
+} Team;
+
+typedef enum
+{
+  Pawn,
+  Knight,
+  Bishop,
+  Rook,
+  Queen,
+  King,
+} PieceType;
+
+class Piece {
+  public:
+    Piece(Team team, PieceType type);
+    Team getTeam();
+    PieceType getType();
+
+  private:
+    Team team;
+    PieceType type;
+};
+
+#endif
